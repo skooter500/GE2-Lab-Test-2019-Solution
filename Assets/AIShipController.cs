de815831cc1236daf9677a74dfd85cd34e3e4377
skooter500 <skooter500@gmail.com>
2019-03-21 16:37:27 +0000

    initial idea

---
+++ Assets/AIShipController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class PatrollingState : State
+{   
+    public override void Enter()
+    {
+        Path path = new Path();
+
+        for(int i = 0; i < 8; i ++)
+        {
+            path.waypoints.add(new Vector3
+    }
+
+
+}
+
+
+public class AIShipController : MonoBehaviour
+{
+    public Base theBase;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
